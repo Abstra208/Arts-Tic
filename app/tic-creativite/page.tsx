6bfb322b1587463bb9925cdd1eecc0bba5dc3f2d
Ludovic Morin <ludovic@abstra208.com>
2025-10-08 21:51:43 -0400

    Mise a jour du lien

--- app/tic-creativite/page.tsx
+++ app/tic-creativite/page.tsx
@@ -5,7 +5,7 @@ export default function TicPage() {
         <div className="flex flex-col items-center justify-center h-screen">
             <h1 className="text-4xl font-bold mb-4">Tic Page</h1>
             <p className="text-lg">This is the Tic page content.</p>
-            <Image src="images/projets/montage_affiche.png" height={500} width={500} className="w-[30%] h-auto" alt="Tic" />
+            <Image src="/images/projets/montage_affiche.png" height={500} width={500} className="w-[30%] h-auto" alt="Tic" />
         </div>
     );
 }
