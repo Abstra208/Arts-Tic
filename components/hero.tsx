19e6673bb36e0de29628fd03925d3280b82ff56d
Ludovic Morin <ludovic@abstra208.com>
2026-05-16 00:15:12 -0400

    Adjust heading size in Hero component for improved responsiveness

--- components/hero.tsx
+++ components/hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
     return (
         <section className="md:h-screen w-full pl-mobile md:pl-desktop text-secondary-text bg-secondary flex flex-col my-15 md:mt-0 gap-8 md:gap-0 md:grid md:grid-cols-[auto_1fr] overflow-hidden">
             <div className="flex flex-col justify-center items-start z-20">
-                <h1 className="text-5xl font-bold tracking-tight">
+                <h1 className="text-4xl md:text-5xl font-bold tracking-tight">
                     Un cerveau qui pense.
                 </h1>
                 <h2 className="mt-1 text-4xl md:text-6xl text-foreground tracking-tight">
